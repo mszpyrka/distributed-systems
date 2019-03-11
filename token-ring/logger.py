baa8177d5e954ba59ef58b0567f08e3850a505e8
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-03-11 01:18:20 +0100

    Added python logger

---
+++ token-ring/logger.py
@@ -0,0 +1,11 @@
+import socket
+
+ip = "224.0.0.1"
+port = 9090
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((ip, port))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(str(data))
