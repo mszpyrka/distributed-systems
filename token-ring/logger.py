d6db9b7b14134a98bd6ae8a2d6b535d483ea0d68
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-03-11 02:21:25 +0100

    Fixed socket options

--- token-ring/logger.py
+++ token-ring/logger.py
@@ -4,6 +4,7 @@ ip = "224.0.0.1"
 port = 9090
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((ip, port))
 
 while True:
